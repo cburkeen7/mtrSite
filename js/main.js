3144efd6ded6f24f5ee98d0405cd2da5ca8ed6ba
cburkeen7 <coleburkeen@icloud.com>
2026-03-11 17:27:30 -0500

    removed /

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ function initDropdown() {
 // After top banner is loaded
 function loadBanners() {
   // Load top banner
-  fetch('/components/topBanner.html')
+  fetch('components/topBanner.html')
     .then(res => res.text())
     .then(html => {
       document.getElementById('header').innerHTML = html;
@@ -37,7 +37,7 @@ function loadBanners() {
     .catch(err => console.error('Error loading top banner:', err));
 
   // Load bottom banner
-  fetch('/components/bottomBanner.html')
+  fetch('components/bottomBanner.html')
     .then(res => res.text())
     .then(html => {
       document.getElementById('footer').innerHTML = html;
